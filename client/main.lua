11b5a49d41294d02f56ceda3ba238af17fa1c049
R1CKY <100082886+R1CKY6@users.noreply.github.com>
2024-02-24 15:08:45 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -65,18 +65,53 @@ Citizen.CreateThread(function()
     end
 end)
 
+RegisterNetEvent('ricky-bossmenu:openMenu')
+AddEventHandler('ricky-bossmenu:openMenu', function(job)
+    OpenBossMenu(job)
+end)
 
 -- Exports
 exports('OpenBossMenu', function(job)
     OpenBossMenu(job)
 end)
 
-RegisterNetEvent('ricky-bossmenu:openMenu')
-AddEventHandler('ricky-bossmenu:openMenu', function(job)
-    OpenBossMenu(job)
-end)
-
 exports('GetSocietyMoney', function(job)
     local money = TriggerServerCallback('ricky-bossmenu:getSocietyMoney', job)
     return money or 0
+end)
+
+exports('AddMoneyToSociety', function(job, amount)
+    TriggerServerEvent('ricky-bossmenu:AddMoneyToSociety', job, amount)
+end)
+
+exports('RemoveMoneyFromSociety', function(job, amount)
+    TriggerServerEvent('ricky-bossmenu:RemoveMoneyFromSociety', job, amount)
+end)
+
+
+local markerWait = 1000
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(markerWait)
+     for a,b in pairs(Config.Job) do 
+        if PlayerData.job.name == a and PlayerData.job.grade >= b.minGrade then 
+            for k,v in pairs(b.coords) do 
+                local distance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v.x, v.y, v.z, true)
+                if distance < 10 then
+                    markerWait = 1
+                    DrawMarker(Config.MarkerSettings.type, v.x, v.y, v.z, 0, 0, 0, 0, 0, 0, Config.MarkerSettings.scale.x,Config.MarkerSettings.scale.y,Config.MarkerSettings.scale.z, Config.MarkerSettings.color.r,Config.MarkerSettings.color.g,Config.MarkerSettings.color.b, Config.MarkerSettings.color.a, Config.MarkerSettings.bump, 0, 0, Config.MarkerSettings.rotate)
+
+                    if distance < 1 then 
+                        ESX.ShowHelpNotification(Config.Locales.press_to_open)
+                        if IsControlJustReleased(0, 38) then 
+                            OpenBossMenu(a)
+                        end
+                    end
+                else
+                    markerWait = 1000
+                end
+            end
+        end
+     end
+   end
 end)
